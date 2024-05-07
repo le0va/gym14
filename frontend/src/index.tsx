0a98d6da02773354486c116dfeaacda4bde5417c
leova <leovkin2003@gmail.com>
2024-05-07 19:48:22 +0300

    ref: Added AppRouter component to root file

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import AppRouter from './navigation/Router';
+import AppRouter from './navigation/AppRouter';
 import './index.css';
 
 const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID || '';

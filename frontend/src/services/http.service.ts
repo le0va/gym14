bb4f27a557d4f4e1bb6508778ee02fa1f4578514
leova <leovkin2003@gmail.com>
2024-08-15 12:44:35 +0300

    bug: Fixed EC2 host address

--- frontend/src/services/http.service.ts
+++ frontend/src/services/http.service.ts
@@ -4,7 +4,7 @@ import { IGetConfig, IPostConfig, IPatchConfig } from "../types";
 class HttpService {
     baseUrl: string;
 
-    constructor(baseUrl = 'http://ec2-51-20-187-125.eu-north-1.compute.amazonaws.com:5000/api') {
+    constructor(baseUrl = 'http://ec2-51-20-248-202.eu-north-1.compute.amazonaws.com:5000/api') {
         this.baseUrl = baseUrl;
     }
 
